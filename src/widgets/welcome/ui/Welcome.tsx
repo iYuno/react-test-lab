062a1ee27da09c1b7555c9d13e4e5b65dd783a0f
Roman Loginov <roma_loginov1@list.ru>
2024-09-29 19:05:40 +0300

    Add responsive design improvements and screen size configurations

--- src/widgets/welcome/ui/Welcome.tsx
+++ src/widgets/welcome/ui/Welcome.tsx
@@ -2,10 +2,10 @@ import { Button } from "@/shared/ui";
 
 export function Welcome() {
   return (
-    <section className="text-center content-center space-y-20 bottom-0 h-[calc(100vh-5rem)] pb-20">
-      <div className="flex flex-col gap-4">
+    <section className="text-center content-center bottom-0 h-[calc(100vh-5rem)] pb-20">
+      <div className="flex flex-col gap-4 lg:mb-20 md:mb-10">
         <h1 className="text-background">Говорят, никогда не поздно <br />сменить профессию</h1>
-        <p className="text-[18px]/[32px] text-background font-light">Сделай круто тестовое задание и у тебя получится</p>
+        <p className="lg:text-[18px]/[32px] md:text-[16px]/[28px] text-background font-light">Сделай круто тестовое задание и у тебя получится</p>
       </div>
       <Button className="text-h5 font-semibold" variant='muted'>
         Проще простого!
