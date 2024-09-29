8c76a560c731e1d4ee32ed08022071ac511549d7
Roman Loginov <roma_loginov1@list.ru>
2024-09-29 18:41:49 +0300

    Add FAQ widget with accordion functionality

---
+++ src/widgets/faq/ui/Faq.tsx
@@ -0,0 +1,23 @@
+
+import { faq } from "@/shared/config/consts";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/shared/ui";
+
+export function Faq() {
+  return (
+    <section className="pt-20 pb-14" id="faq">
+      <h2 className="text-center">Вопросы и ответы</h2>
+      <Accordion type="single" collapsible className="grid grid-cols-12 mt-10 gap-x-8">
+        {
+          faq.map(({ id, question, answer }, index) => (
+            <AccordionItem value={id.toString()} key={id} className="col-start-2 col-span-10 border-t border-input-border">
+              <AccordionTrigger className="hover:text-primary font-semibold text-[18px]/[32px]">{question}</AccordionTrigger>
+              <AccordionContent className="pr-8 text-[18px]/[32px] text-input-label">
+                {answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))
+        }
+      </Accordion>
+    </section>
+  )
+}
