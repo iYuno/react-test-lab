77ddfcc278e90e0ded55dcdd8fbd00f2205bdb57
Roman Loginov <roma_loginov1@list.ru>
2024-09-29 18:55:46 +0300

    Implement smooth scrolling for header navigation links

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const smoothScroll = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute('href')?.substring(1);
+  if (targetId) {
+    const targetElement = document.getElementById(targetId);
+    targetElement?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'center',
+    });
+  }
+};
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,3 +1,4 @@
+import { smoothScroll } from "@/shared/lib/utils";
 import { Logo } from "@/shared/ui";
 
 export function Header() {
@@ -6,10 +7,10 @@ export function Header() {
     <header className="w-full flex h-20 justify-between z-20 items-center">
       <Logo />
       <nav className="h-7 font-light content-center *:px-3 flex text-[16px]/[28px] *:text-background *:decoration-transparent *:cursor-pointer *:underline-offset-4 *:transition-all *:duration-75 *:ease-out">
-        <a href='#instructions' className="hover:decoration-background">Как это работает</a>
-        <a href='#reviews' className="hover:decoration-background">3-й блок</a>
-        <a href='#faq' className="hover:decoration-background">Вопросы и ответы</a>
-        <a href='#testForm' className="hover:decoration-background">Форма</a>
+        <a href='#instructions' onClick={smoothScroll} className="hover:decoration-background">Как это работает</a>
+        <a href='#reviews' onClick={smoothScroll} className="hover:decoration-background">3-й блок</a>
+        <a href='#faq' onClick={smoothScroll} className="hover:decoration-background">Вопросы и ответы</a>
+        <a href='#testForm' onClick={smoothScroll} className="hover:decoration-background">Форма</a>
       </nav>
     </header>
   )
