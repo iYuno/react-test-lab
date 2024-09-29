4159b41e88f3d61e31d615c51a2772f822eefe76
Roman Loginov <roma_loginov1@list.ru>
2024-09-29 18:42:37 +0300

    Refactor LandingPage to include new widget components and remove inline content

--- src/pages/landing/ui/Page.tsx
+++ src/pages/landing/ui/Page.tsx
@@ -1,19 +1,23 @@
-import { Button } from "@/shared/ui";
+import { Faq } from "@/widgets/faq";
+import { Instructions } from "@/widgets/instructions";
+import { Reviews } from "@/widgets/reviews";
+import { Survey } from "@/widgets/survey";
+import { Welcome } from "@/widgets/welcome";
+import { TestForm } from "@/features/testForm";
+
+
 
 export function LandingPage() {
 
   return (
     <>
       <main className="z-20">
-        <section className="text-center content-center space-y-20 mt-[calc(50vh-240px)]">
-          <div className="flex flex-col gap-4">
-            <h1 className="text-background">Говорят, никогда не поздно <br />сменить профессию</h1>
-            <p className="text-[18px]/[32px] text-background">Сделай круто тестовое задание и у тебя получится</p>
-          </div>
-          <Button className="text-h5 font-bold" variant='muted'>
-            Проще простого!
-          </Button>
-        </section>
+        <Welcome />
+        <Instructions />
+        <Reviews />
+        <Faq />
+        <Survey />
+        <TestForm />
       </main>
       <div className="absolute left-0 right-0 h-screen z-10 bg-primary/80" />
       <img className="absolute left-0 w-screen h-screen object-cover z-0" src="public\bg_main.png" />
