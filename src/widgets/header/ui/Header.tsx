33906b9f2db27e6c3423e8d5ae25ef093f1261ad
Roman Loginov <roma_loginov1@list.ru>
2024-10-09 20:12:05 +0300

    Refactor `Header.tsx` for improved readability and consistency in code style

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -7,8 +7,10 @@ export function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
 
   const toggleMenu = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    if (isMenuOpen) { smoothScroll(event) }
-    setMenuOpen(prevState => !prevState);
+    if (isMenuOpen) {
+      smoothScroll(event);
+    }
+    setMenuOpen((prevState) => !prevState);
   };
 
   useEffect(() => {
@@ -19,20 +21,22 @@ export function Header() {
         setIsScrolled(false);
       }
     };
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     <header className="xs:fixed lg:relative self-center xs:w-[calc(100%-32px)] md:w-[728px] lg:w-full flex xs:flex-col lg:flex-row h-20 lg:justify-between z-30 items-center">
       <div className="xs:flex xs:justify-between h-full lg:h-auto xs:w-full xs:items-center z-30">
-        <Logo className={cn(
-          isScrolled || isMenuOpen ? "text-black" : "text-background",
-          "lg:text-background transition-all duration-100"
-        )} />
+        <Logo
+          className={cn(
+            isScrolled || isMenuOpen ? "text-black" : "text-background",
+            "lg:text-background"
+          )}
+        />
         <Burger
           isOpen={isMenuOpen}
           toggleMenu={toggleMenu}
@@ -42,78 +46,62 @@ export function Header() {
             "lg:hidden -right-4"
           )}
         />
-        <span className={cn(
-          "absolute lg:hidden inset-0 -mx-[calc(100vw-100%)] transition-all ease-out duration-150",
-          isScrolled && !isMenuOpen ? "shadow-lg z-10" : "bg-transparent",
-          isScrolled && "bg-white"
-        )} />
+        <span
+          className={cn(
+            "absolute lg:hidden inset-0 -mx-[calc(100vw-100%)] transition-all ease-out duration-150",
+            isScrolled && !isMenuOpen ? "shadow-lg z-10" : "bg-transparent",
+            isScrolled && "bg-white"
+          )}
+        />
       </div>
-      <nav
-        className={cn(
-          "xs:absolute xs:top-20 lg:top-0 lg:static",
-          "xs:w-full lg:w-auto lg:h-7",
-          "lg:content-center lg:px-3 xs:flex-col lg:flex-row flex",
-          "text-nowrap font-light text-[16px]/[28px]",
-          "lg:text-background xs:text-input-label-active",
-          "xs:*:border-b lg:*:border-none",
-          "decoration-transparent underline-offset-4",
-          "transition-all duration-75 ease-out",
-          "cursor-pointer"
-        )}
-      >
+      <nav className="cursor-pointer xs:*:border-b lg:*:border-none lg:h-7 xs:absolute xs:top-20 lg:top-0 lg:static xs:w-full lg:w-auto text-nowrap font-light lg:content-center lg:*:px-3 flex xs:flex-col lg:flex-row text-[16px]/[28px] lg:*:text-background xs:*:text-input-label-active *:decoration-transparent *:cursor-pointer *:underline-offset-4 transition-all *:duration-75 *:ease-out">
         <a
-          href='#instructions'
+          href="#instructions"
           onClick={toggleMenu}
           className={cn(
-            "hover:decoration-background max-lg:hover:text-black max-lg:hover:border-b-black",
-            "xs:border-t lg:border-none xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
-            isMenuOpen ? '' : 'xs:hidden lg:block'
+            "hover:decoration-background max-lg:hover:text-black max-lg:hover:border-b-black xs:border-t lg:border-none xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
+            isMenuOpen ? "" : "xs:hidden lg:block"
           )}
         >
           Как это работает
         </a>
         <a
-          href='#reviews'
+          href="#reviews"
           onClick={toggleMenu}
           className={cn(
-            "lg:hover:decoration-black max-lg:hover:text-black max-lg:hover:border-b-black",
-            "xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
-            isMenuOpen ? '' : 'xs:hidden lg:block'
+            "lg:hover:decoration-background max-lg:hover:text-black max-lg:hover:border-b-black xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
+            isMenuOpen ? "" : "xs:hidden lg:block"
           )}
         >
           3-й блок
         </a>
         <a
-          href='#faq'
+          href="#faq"
           onClick={toggleMenu}
           className={cn(
-            "hover:decoration-background max-lg:hover:text-black max-lg:hover:border-b-black",
-            "xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
-            isMenuOpen ? '' : 'xs:hidden lg:block'
+            "hover:decoration-background max-lg:hover:text-black max-lg:hover:border-b-black xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
+            isMenuOpen ? "" : "xs:hidden lg:block"
           )}
         >
           Вопросы и ответы
         </a>
         <a
-          href='#testForm'
+          href="#testForm"
           onClick={toggleMenu}
           className={cn(
-            "hover:decoration-background max-lg:hover:text-black max-lg:hover:border-b-black",
-            "xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
-            isMenuOpen ? '' : 'xs:hidden lg:block'
+            "hover:decoration-background max-lg:hover:text-black max-lg:hover:border-b-black xs:w-full lg:w-auto xs:py-4 lg:py-0 z-30",
+            isMenuOpen ? "" : "xs:hidden lg:block"
           )}
         >
           Форма
         </a>
         <span
           className={cn(
-            "pointer-events-none z-20 border-none lg:hidden inset-0 absolute",
-            "-mx-[calc(100vw-100%)] h-screen -top-20",
-            "transition-all ease-out duration-150",
-            isMenuOpen ? 'bg-white' : 'bg-transparent'
+            "pointer-events-none z-20 border-none lg:hidden inset-0 absolute -mx-[calc(100vw-100%)] h-screen -top-20 transition-all ease-out duration-150",
+            isMenuOpen ? "bg-white" : "bg-transparent"
           )}
         />
       </nav>
     </header>
-  )
+  );
 }
