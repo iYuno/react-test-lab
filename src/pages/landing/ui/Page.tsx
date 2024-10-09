2de284b32ff81c243f60539c16e2c6c67c8276a0
Roman Loginov <roma_loginov1@list.ru>
2024-10-09 22:16:28 +0300

    Implement lazy loading for widgets using React Suspense in LandingPage

--- src/pages/landing/ui/Page.tsx
+++ src/pages/landing/ui/Page.tsx
@@ -1,29 +1,31 @@
+import { Suspense, lazy } from "react";
 import { createPortal } from "react-dom";
-import { Faq } from "@/widgets/faq";
-import { Instructions } from "@/widgets/instructions";
-import { Reviews } from "@/widgets/reviews";
-import { Survey } from "@/widgets/survey";
-import { Welcome } from "@/widgets/welcome";
-import { TestForm } from "@/features/testForm";
 
+const Faq = lazy(() => import("@/widgets/faq").then(module => ({ default: module.Faq })));
+const Instructions = lazy(() => import("@/widgets/instructions").then(module => ({ default: module.Instructions })));
+const Reviews = lazy(() => import("@/widgets/reviews").then(module => ({ default: module.Reviews })));
+const Survey = lazy(() => import("@/widgets/survey").then(module => ({ default: module.Survey })));
+const Welcome = lazy(() => import("@/widgets/welcome").then(module => ({ default: module.Welcome })));
+const TestForm = lazy(() => import("@/features/testForm").then(module => ({ default: module.TestForm })));
 
 
 export function LandingPage() {
-
   const rootElement = document.getElementById('root');
 
   return (
     <>
       <main className="z-20">
-        <Welcome />
-        <Instructions />
-        <Reviews />
-        <Faq />
-        <Survey />
-        <TestForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Welcome />
+          <Instructions />
+          <Reviews />
+          <Faq />
+          <Survey />
+          <TestForm />
+        </Suspense>
       </main>
       <div className="absolute left-0 right-0 h-screen z-10 bg-primary/80 -mx-[calc(100vw-100%)]" />
       {rootElement && createPortal(<img className="absolute left-0 w-screen h-screen object-cover z-0 " src="bg_main.png" />, rootElement)}
     </>
-  )
+  );
 }
